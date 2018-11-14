8ecfb2f9ac9eb466bd8475c9ee45bac8659fab5d
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-11-14 09:36:50 -0700

    add project to repo, create wpf, xamarin, avalonia, ooui, sharedviewmodel, and test projects

---
+++ Crossplatform_Whiskipe/Ooui_Whiskipe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ooui_Whiskipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
